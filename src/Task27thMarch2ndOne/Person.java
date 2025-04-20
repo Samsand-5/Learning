594c40c34e384fa4920956a1b85f0ea874c39156
coder <soumyodipsutra@gmail.com>
2025-04-20 17:29:53 +0530

    working

--- src/Task27thMarch2ndOne/Person.java
+++ src/Task27thMarch2ndOne/Person.java
@@ -13,8 +13,19 @@ public class Person {
         System.out.println(name + ",  " + age);
     }
 
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public void setName(String name) { this.name = name; }
-    public void setAge(int age) { this.age = age; }
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
---
+++ src/Task27thMarch2ndOne/Student.java
@@ -0,0 +1,24 @@
+package Task27thMarch2ndOne;
+
+public class Student extends Person {
+    private int studentId;
+
+    public Student(String name, int age, int studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+
+    public void displayStudentInfo() {
+        super.displayInfo();
+        System.out.println(" " + studentId);
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
+
