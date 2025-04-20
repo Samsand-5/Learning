18a49d02d41a9ff777b101b1801b346a6ea02639
coder <soumyodipsutra@gmail.com>
2025-04-20 17:13:48 +0530

    working

--- src/Task27thMarch/Employee.java
+++ src/Task27thMarch/Employee.java
@@ -1,4 +1,16 @@
 package Task27thMarch;
 
 public class Employee {
+
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void employeeIngo(){
+        System.out.println(this.name+" "+this.salary);
+    }
 }
