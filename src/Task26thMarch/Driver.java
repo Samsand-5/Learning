8a2fcaa55cdb67237eda7857ae54c1f4997a0199
coder <soumyodipsutra@gmail.com>
2025-04-20 17:10:14 +0530

    working

--- src/Task26thMarch/Driver.java
+++ src/Task26thMarch/Driver.java
@@ -1,4 +1,8 @@
 package Task26thMarch;
 
 public class Driver {
+    public static void main(String args[]){
+        Student st=new Student("Soumyodip",22,1);
+        st.displayStudentDetails();
+    }
 }
