7662243f8946d18a5087f828db0fac92652a4c59
coder <soumyodipsutra@gmail.com>
2025-04-20 17:08:29 +0530

    working

--- src/Task26thMarch/Person.java
+++ src/Task26thMarch/Person.java
@@ -1,11 +1,16 @@
 package Task26thMarch;
 
 public class Person {
+
     private String name;
-    private int id;
-    Person(String name,int id){
+    private int age;
+
+    Person(String name,int age){
         this.name=name;
-        this.id=id;
+        this.age=age;
     }
 
+    public void displayPersonDetails(){
+        System.out.print(this.name+"\t"+this.age);
+    }
 }
--- src/Task26thMarch/Student.java
+++ src/Task26thMarch/Student.java
@@ -1,4 +1,17 @@
 package Task26thMarch;
 
-public class Student {
+public class Student extends Person {
+
+    private int studentID;
+
+    public Student(String name, int age, int studentID) {
+        super(name,age);
+        this.studentID = studentID;
+    }
+
+    public void displayStudentDetails() {
+        displayPersonDetails();
+        System.out.println("\t"+this.studentID);
+    }
+
 }
