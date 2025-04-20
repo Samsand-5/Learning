51988925a840f67df88d207d4ec551ff2cec52be
coder <soumyodipsutra@gmail.com>
2025-04-20 17:28:39 +0530

    working

---
+++ src/Task27thMarch2ndOne/Person.java
@@ -0,0 +1,20 @@
+package Task27thMarch2ndOne;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println(name + ",  " + age);
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+}
