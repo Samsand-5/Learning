3a4bf48f034d4244f913aa94888555fad0219a14
coder <soumyodipsutra@gmail.com>
2025-04-20 17:17:09 +0530

    working

--- src/Task27thMarch/FullTimeEmployee.java
+++ src/Task27thMarch/FullTimeEmployee.java
@@ -1,4 +1,16 @@
 package Task27thMarch;
 
-public class FullTimeEmployee {
+public class FullTimeEmployee extends Employee{
+
+    private int bonus;
+
+    public FullTimeEmployee(String name, double salary, int bonus) {
+        super(name, salary);
+        this.bonus=bonus;
+    }
+
+    public void FullTimeEmployeeInfo(){
+        employeeIngo();
+        System.out.println(this.bonus);
+    }
 }
