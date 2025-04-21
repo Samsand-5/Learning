40b1640c20dc9ec3145c171f5cbf0ee0fb614e8a
coder <soumyodipsutra@gmail.com>
2025-04-21 19:38:53 +0530

    working

--- src/Task28thMarch2ndOne/Main.java
+++ src/Task28thMarch2ndOne/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         Payment payMoney1=new CreditCardPayment(300.00,1);
-        System.out.println("Credit Card " + payMoney1.processPayment());
+        System.out.println("Credit Card " + payMoney1.processPayment() + " on Transaction id " + payMoney1.getTransactionId());
 
         Payment payMoney2=new PayPalPayment(300.00,2);
-        System.out.println("Credit Card " + payMoney2.processPayment());
+        System.out.println("Credit Card " + payMoney2.processPayment() + " on Transaction id " + payMoney2.getTransactionId());
     }
 }
