317549593c41f5bfef129c06f8eef25b74c93683
coder <soumyodipsutra@gmail.com>
2025-04-22 22:04:12 +0530

    working

--- src/Task28thMarch1stOne/Main.java
+++ src/Task28thMarch1stOne/Main.java
@@ -2,10 +2,18 @@ package Task28thMarch1stOne;
 
 public class Main {
     public static void main(String[] args) {
-        BasicCalci bc=new BasicCalci();
-        System.out.println(bc.add(1,2));
-        System.out.println(bc.sub(1,2));
+        CalciVer1 c1 = new CalciVer1();
+        System.out.println("Add " + c1.add(1, 2));
+        System.out.println("Sub "+ c1.sub(1,2));
+        System.out.println("Power " + c1.power(2, 3));
 
+        System.out.println();
 
+        CalciVer2 c2 = new CalciVer2();
+        System.out.println("Add " + c2.add(1, 2));
+        System.out.println("Sub " + c2.sub(1, 2));
+        System.out.println("Square " + c2.square(2));
+        System.out.println("Cube " + c2.cube(2));
     }
 }
+
