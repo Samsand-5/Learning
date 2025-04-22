e2fb623cce8433682e979d0361a89a516469e67f
coder <soumyodipsutra@gmail.com>
2025-04-22 21:56:24 +0530

    working

--- src/Task28thMarch1stOne/Main.java
+++ src/Task28thMarch1stOne/Main.java
@@ -1,4 +1,11 @@
 package Task28thMarch1stOne;
 
 public class Main {
+    public static void main(String[] args) {
+        BasicCalci bc=new BasicCalci();
+        System.out.println(bc.add(1,2));
+        System.out.println(bc.sub(1,2));
+
+
+    }
 }
