b47dec1af34b306691eb82cfe996f3edc85877ef
coder <soumyodipsutra@gmail.com>
2025-04-22 22:08:14 +0530

    working

--- src/Task28thMarch1stOne/CalciVer1.java
+++ src/Task28thMarch1stOne/CalciVer1.java
@@ -2,6 +2,14 @@ package Task28thMarch1stOne;
 
 public class CalciVer1 extends AbstractCalciVer1{
 
+    int add(int a,int b){
+        return a+b;
+    }
+
+    int sub(int a,int b){
+        return Math.abs(a-b);
+    }
+
     @Override
     double power(double a, double b) {
         return Math.pow(a,b);
--- src/Task28thMarch1stOne/CalciVer2.java
+++ src/Task28thMarch1stOne/CalciVer2.java
@@ -2,6 +2,14 @@ package Task28thMarch1stOne;
 
 public class CalciVer2 extends AbstractCalciVer2{
 
+    int add(int a,int b){
+        return a+b;
+    }
+
+    int sub(int a,int b){
+        return Math.abs(a-b);
+    }
+
     @Override
     double square(double a) {
         return a*a;
