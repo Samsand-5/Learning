580b95a3bd592cab126cb4deaa0b836aa415f639
coder <soumyodipsutra@gmail.com>
2025-04-22 21:45:00 +0530

    working

---
+++ src/Task28thMarch1stOne/Calculator.java
@@ -0,0 +1,12 @@
+package Task28thMarch1stOne;
+
+abstract public class Calculator {
+
+    int add(int a,int b){
+        return a+b;
+    }
+
+    int sub(int a,int b){
+        return Math.abs(a-b);
+    }
+}
