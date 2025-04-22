5228aaa3c5402af36832a7a445a069da3b0ad7fe
coder <soumyodipsutra@gmail.com>
2025-04-22 22:06:36 +0530

    working

--- src/Task28thMarch1stOne/Main.java
+++ src/Task28thMarch1stOne/Main.java
@@ -14,6 +14,12 @@ public class Main {
         System.out.println("Sub " + c2.sub(1, 2));
         System.out.println("Square " + c2.square(2));
         System.out.println("Cube " + c2.cube(2));
+
+        System.out.println();
+
+        BasicCalci bc=new BasicCalci();
+        System.out.println("Add " + bc.add(1,2));
+        System.out.println("Sub " + bc.sub(1,2));
     }
 }
 
