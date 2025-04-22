36c269b6e7b5d80cac633500afef21610820e18b
coder <soumyodipsutra@gmail.com>
2025-04-22 22:00:55 +0530

    working

---
+++ src/Task28thMarch1stOne/CalciVer2.java
@@ -0,0 +1,14 @@
+package Task28thMarch1stOne;
+
+public class CalciVer2 extends AbstractCalciVer2{
+
+    @Override
+    double square(double a) {
+        return a*a;
+    }
+
+    @Override
+    double cube(double a) {
+        return a*a*a;
+    }
+}
