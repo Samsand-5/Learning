2c2bbbe1b7674fc231844df85d56f3825a0f1e48
coder <soumyodipsutra@gmail.com>
2025-04-22 21:48:45 +0530

    working

--- src/Task28thMarch1stOne/CalciVer2.java
+++ src/Task28thMarch1stOne/CalciVer2.java
@@ -1,5 +1,11 @@
 package Task28thMarch1stOne;
 
 public class CalciVer2 extends Calculator{
+    double square(double a){
+        return a*a;
+    }
 
+    double cube(double a){
+        return a*a*a;
+    }
 }
