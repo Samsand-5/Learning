f7832255e8ffd3ccad526581323feb4b4ae4a0a8
coder <soumyodipsutra@gmail.com>
2025-04-22 23:45:14 +0530

    working

--- src/Task28thMarch1stOne/BasicCalci.java
+++ src/Task28thMarch1stOne/BasicCalci.java
@@ -2,10 +2,12 @@ package Task28thMarch1stOne;
 
 public class BasicCalci extends Calculator{
 
+    @Override
     int add(int a, int b) {
         return a+b;
     }
 
+    @Override
     int sub(int a, int b) {
         return Math.abs(a-b);
     }
