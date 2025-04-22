0ca5ab816889a050c98d7015d45a07fec8e28239
coder <soumyodipsutra@gmail.com>
2025-04-22 21:47:29 +0530

    working

--- src/Task28thMarch1stOne/CalciVer1.java
+++ src/Task28thMarch1stOne/CalciVer1.java
@@ -1,6 +1,6 @@
 package Task28thMarch1stOne;
 
-abstract public class CalciVer1 {
+abstract public class CalciVer1 extends Calculator{
     double power(double a,double b){
         return Math.pow(a,b);
     }
