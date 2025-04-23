c7f9d5d3af03112a6ee2dba73c075221bc4c2c81
coder <soumyodipsutra@gmail.com>
2025-04-23 19:56:51 +0530

    working

---
+++ src/Task14thApril/Passenger.java
@@ -0,0 +1,15 @@
+package Task14thApril;
+
+public class Passenger {
+
+    String name;
+
+    Passenger(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
