de451eb30e891306f427a41e2004f81083670980
coder <soumyodipsutra@gmail.com>
2025-04-23 19:59:46 +0530

    working

--- src/Task14thApril/Flight.java
+++ src/Task14thApril/Flight.java
@@ -11,4 +11,15 @@ public class Flight<T extends Passenger> {
         this.flightNumber = flightNumber;
         this.passengers = new ArrayList<>();
     }
+
+    void addPassenger(T passenger) {
+        passengers.add(passenger);
+    }
+
+    void showPassengers() {
+        System.out.println("Passengers in flight " + flightNumber + ":");
+        for (T passenger : passengers) {
+            System.out.println(passenger);
+        }
+    }
 }
