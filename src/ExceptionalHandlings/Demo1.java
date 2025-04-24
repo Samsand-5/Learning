8783cd6e5fee8e665f2d1f96d97612d889f063d2
coder <soumyodipsutra@gmail.com>
2025-04-24 20:55:07 +0530

    working

---
+++ src/ExceptionalHandlings/Demo1.java
@@ -0,0 +1,20 @@
+package ExceptionalHandlings;
+
+import java.util.*;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        try(Scanner scan = new Scanner(System.in)) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x/y);
+        }
+        catch(Exception e) {
+            System.out.println("Exception handled");
+        }
+        finally {
+            System.out.println("finally block");
+        }
+    }
+}
