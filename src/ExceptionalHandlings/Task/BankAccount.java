503b9941174fc4365c733d3521471ed7c1c76a51
coder <soumyodipsutra@gmail.com>
2025-04-24 23:39:23 +0530

    working

--- src/ExceptionalHandlings/Task/BankAccount.java
+++ src/ExceptionalHandlings/Task/BankAccount.java
@@ -1,4 +1,17 @@
 package ExceptionalHandlings.Task;
 
 public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double amount){
+        this.balance=amount;
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if(amount>balance){
+            throw new Exception("Insufficient Balance");
+        }
+        balance-=amount;
+    }
 }
