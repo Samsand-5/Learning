26a9d0d45ee8a994934348a6e6a8dc1890e7be19
coder <soumyodipsutra@gmail.com>
2025-04-27 17:59:44 +0530

    working

--- src/JDBCTutorial/Main.java
+++ src/JDBCTutorial/Main.java
@@ -1,7 +1,18 @@
 package JDBCTutorial;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            String url = "jdbc:mysql://localhost:3306/MyWork";
+            String username = "root";
+            String password = "13soumyodip2002";
+            Connection c = DriverManager.getConnection(url, username, password);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
