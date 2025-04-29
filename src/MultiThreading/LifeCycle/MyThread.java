3c80e2c938c1f478e275c661a1435e9244ef1e7f
coder <soumyodipsutra@gmail.com>
2025-04-29 18:18:08 +0530

    working

--- src/MultiThreading/LifeCycle/MyThread.java
+++ src/MultiThreading/LifeCycle/MyThread.java
@@ -1,5 +1,16 @@
 package MultiThreading.LifeCycle;
 
-public class MyThread {
+public class MyThread extends Thread{
 
+    @Override
+    public void run(){
+
+    }
+
+    public static void main(String[] args) {
+        MyThread t1=new MyThread();
+        System.out.println(t1.getState());
+        t1.start();
+        System.out.println(t1.getState());
+    }
 }
