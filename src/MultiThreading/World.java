884d4963f817f6d463320c7494fe8a847c835b99
coder <soumyodipsutra@gmail.com>
2025-04-29 18:06:10 +0530

    working

--- src/MultiThreading/World.java
+++ src/MultiThreading/World.java
@@ -5,5 +5,8 @@ public class World extends Thread{
     @Override
     public void run(){
         super.run();
+        for(int i=0;i<1000;i++){
+            System.out.println("world");
+        }
     }
 }
