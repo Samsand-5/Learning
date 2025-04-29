ef2122998260edb5ff9104c042ea577f08afc95e
coder <soumyodipsutra@gmail.com>
2025-04-29 19:36:51 +0530

    working

--- src/MultiThreading/Synchronization/MyThread2.java
+++ src/MultiThreading/Synchronization/MyThread2.java
@@ -1,4 +1,10 @@
 package MultiThreading.Synchronization;
 
 public class MyThread2 extends Thread{
+
+    private Counter counter;
+
+    public  MyThread2(Counter counter){
+        this.counter=counter;
+    }
 }
--- src/MultiThreading/Synchronization/Test.java
+++ src/MultiThreading/Synchronization/Test.java
@@ -3,5 +3,6 @@ package MultiThreading.Synchronization;
 public class Test {
     public static void main(String[] args) {
         Counter counter=new Counter();
+        MyThread2 t2=new MyThread2(counter);
     }
 }
