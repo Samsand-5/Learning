a6f89829096e48506caf1cff1a465a07be45b008
coder <soumyodipsutra@gmail.com>
2025-04-29 19:39:08 +0530

    working

--- src/MultiThreading/Synchronization/MyThread2.java
+++ src/MultiThreading/Synchronization/MyThread2.java
@@ -7,4 +7,11 @@ public class MyThread2 extends Thread{
     public  MyThread2(Counter counter){
         this.counter=counter;
     }
+
+    @Override
+    public void run() {
+        for(int i=0;i<1000;i++){
+            counter.increment();
+        }
+    }
 }
