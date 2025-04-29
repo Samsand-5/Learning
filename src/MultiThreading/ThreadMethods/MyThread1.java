e1167b7185cbd2cc9d0850be75611b57b4c9a1d1
coder <soumyodipsutra@gmail.com>
2025-04-29 18:45:09 +0530

    working

--- src/MultiThreading/ThreadMethods/MyThread1.java
+++ src/MultiThreading/ThreadMethods/MyThread1.java
@@ -1,7 +1,14 @@
 package MultiThreading.ThreadMethods;
 
-public class MyThread1 {
-    public static void main(String[] args) {
+public class MyThread1 extends Thread{
 
+    @Override
+    public void start() {
+        System.out.println("Thread is Running");
+    }
+
+    public static void main(String[] args) {
+        MyThread1 t1=new MyThread1();
+        t1.start();
     }
 }
