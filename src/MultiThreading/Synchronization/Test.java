6332dc93806aa87cb9d71a355fddd74ddce82daa
coder <soumyodipsutra@gmail.com>
2025-04-29 19:42:26 +0530

    working

--- src/MultiThreading/Synchronization/Test.java
+++ src/MultiThreading/Synchronization/Test.java
@@ -3,6 +3,17 @@ package MultiThreading.Synchronization;
 public class Test {
     public static void main(String[] args) {
         Counter counter=new Counter();
+        MyThread2 t1=new MyThread2(counter);
         MyThread2 t2=new MyThread2(counter);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        }
+        catch (Exception e){
+
+        }
+        System.out.println(counter.getCount());
     }
 }
