7d11c2be3cdcaa5a14e18a3609ddfda3b6542e1b
coder <soumyodipsutra@gmail.com>
2025-04-29 22:11:16 +0530

    working

---
+++ src/MultiThreading/Locks/ReadWriteCounter.java
@@ -0,0 +1,18 @@
+package MultiThreading.Locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteCounter {
+
+    private int count=0;
+
+    private final ReadWriteLock lock=new ReentrantReadWriteLock();
+
+    private final Lock readLock=lock.readLock();
+
+    private final Lock writeLock=lock.writeLock();
+
+
+}
