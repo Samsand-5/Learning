14b1345c0e87313c081848248795c55c8928e7d8
coder <soumyodipsutra@gmail.com>
2025-04-29 23:00:29 +0530

    working

--- src/MultiThreading/LambdaExpression/LambdaExpressionExample.java
+++ src/MultiThreading/LambdaExpression/LambdaExpressionExample.java
@@ -2,6 +2,10 @@ package MultiThreading.LambdaExpression;
 
 public class LambdaExpressionExample {
     public static void main(String[] args) {
-
+        Runnable runnable= () ->{
+            System.out.println("Hello");
+        };
+        Thread t1=new Thread(runnable);
+        t1.start();
     }
 }
