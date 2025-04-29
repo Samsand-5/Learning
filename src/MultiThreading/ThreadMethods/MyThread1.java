9c4829edcee6a9178eed6f8a50fa5dec58a72c54
coder <soumyodipsutra@gmail.com>
2025-04-29 18:49:26 +0530

    working

--- src/MultiThreading/ThreadMethods/MyThread1.java
+++ src/MultiThreading/ThreadMethods/MyThread1.java
@@ -5,6 +5,15 @@ public class MyThread1 extends Thread{
     @Override
     public void start() {
         System.out.println("Thread is Running");
+        for(int i=0;i<14;i++){
+            try{
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+        }
     }
 
     public static void main(String[] args) {
