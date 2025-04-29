e24107c3f220429f006bb6072162511d5c8a7222
coder <soumyodipsutra@gmail.com>
2025-04-29 19:07:02 +0530

    working

--- src/MultiThreading/ThreadMethods/MyThread1.java
+++ src/MultiThreading/ThreadMethods/MyThread1.java
@@ -19,32 +19,46 @@ public class MyThread1 extends Thread{
 //        }
 //    }
 
+//    @Override
+//    public void run() {
+//        for(int i=0;i<5;i++){
+//            System.out.println(Thread.currentThread().getName()+" -Priority"+
+//                    Thread.currentThread().getPriority()+" -count"+i);
+//            try{
+//                Thread.sleep(100);
+//            }
+//            catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
     @Override
-    public void run() {
-        for(int i=0;i<5;i++){
-            System.out.println(Thread.currentThread().getName()+" -Priority"+
-                    Thread.currentThread().getPriority()+" -count"+i);
-            try{
-                Thread.sleep(100);
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
+    public void run(){
+        try {
+            Thread.sleep(1000);
+            System.out.println("Thread");
+        } catch (InterruptedException e) {
+            System.out.println("Thread interupted "+e);
         }
     }
 
     public static void main(String[] args) throws InterruptedException{
-        MyThread1 t1=new MyThread1("LOW");
-        MyThread1 t2=new MyThread1("MEDIUM");
-        MyThread1 t3=new MyThread1("HIGH");
+//        MyThread1 t1=new MyThread1("LOW");
+//        MyThread1 t2=new MyThread1("MEDIUM");
+//        MyThread1 t3=new MyThread1("HIGH");
+////        t1.start();
+////        t1.join();
+//        System.out.println("Hello");
+//        t1.setPriority(Thread.MIN_PRIORITY);
+//        t2.setPriority(Thread.NORM_PRIORITY);
+//        t3.setPriority(Thread.MAX_PRIORITY);
 //        t1.start();
-//        t1.join();
-        System.out.println("Hello");
-        t1.setPriority(Thread.MIN_PRIORITY);
-        t2.setPriority(Thread.NORM_PRIORITY);
-        t3.setPriority(Thread.MAX_PRIORITY);
+//        t2.start();
+//        t3.start();
+
+        MyThread1 t1=new MyThread1("MESSI");
         t1.start();
-        t2.start();
-        t3.start();
+        t1.interrupt();
     }
 }
