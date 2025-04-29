0efcd7dbca365b69680cc98039ff4c7f5a38f8e1
coder <soumyodipsutra@gmail.com>
2025-04-29 19:26:14 +0530

    working

--- src/MultiThreading/Synchronization/Counter.java
+++ src/MultiThreading/Synchronization/Counter.java
@@ -1,4 +1,14 @@
 package MultiThreading.Synchronization;
 
 public class Counter {
+
+    private int count=0;
+
+    public void increment(){
+        count++;
+    }
+
+    public int getCount(){
+        return  count;
+    }
 }
