19468c661f9830e3334ca2081f10b974fbd3d473
coder <soumyodipsutra@gmail.com>
2025-04-29 07:03:03 +0530

    working

---
+++ src/UberRentalSystem/User.java
@@ -0,0 +1,13 @@
+package UberRentalSystem;
+
+public abstract class User {
+    protected String id;
+    protected String name;
+
+    public User(String id,String name) {
+        this.id=id;
+        this.name=name;
+    }
+
+    abstract void showProfiles();
+}
