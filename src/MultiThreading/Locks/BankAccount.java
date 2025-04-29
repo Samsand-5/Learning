186de0482eadbf34c248cc174428299f135e80a9
coder <soumyodipsutra@gmail.com>
2025-04-29 21:19:39 +0530

    working

--- src/MultiThreading/Locks/BankAccount.java
+++ src/MultiThreading/Locks/BankAccount.java
@@ -1,28 +1,43 @@
 package MultiThreading.Locks;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private int balance=100;
 
-    public synchronized void withDraw(int amount){
+    private final Lock lock=new ReentrantLock();
+
+    public void withDraw(int amount){
         System.out.println(Thread.currentThread().getName()+
                 " withdraw "+amount);
-        if(balance>=amount){
-            System.out.println(Thread.currentThread().getName()+
-                    " proceeding withdraw");
-            try {
-                Thread.sleep(3000);
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                if(balance>=amount) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() +
+                                " proceeding withdraw");
+                        Thread.sleep(3000);
+                        balance -= amount;
+                        System.out.println(Thread.currentThread().getName() +
+                                " completed withDrawl, Remaining balance: " + balance);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    finally {
+
+                    }
+                }
+                else{
+                    System.out.println(Thread.currentThread().getName()+
+                            " insufficient balance");
+                }
             }
-            catch (InterruptedException e){
-                throw new RuntimeException(e);
-            }
-            balance-=amount;
-            System.out.println(Thread.currentThread().getName()+
-                    " completed withDrawl, Remaining balance: "+balance);
-        }
-        else{
-            System.out.println(Thread.currentThread().getName()+
-                    " insufficient balance");
+        }catch (Exception e){
+
         }
     }
 }
