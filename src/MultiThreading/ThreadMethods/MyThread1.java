bd1d1d4a414ffc9864e2099b3faca8edc59619a9
coder <soumyodipsutra@gmail.com>
2025-04-29 18:58:33 +0530

    working

--- src/MultiThreading/ThreadMethods/MyThread1.java
+++ src/MultiThreading/ThreadMethods/MyThread1.java
@@ -2,24 +2,42 @@ package MultiThreading.ThreadMethods;
 
 public class MyThread1 extends Thread{
 
+    public MyThread1(String name){
+        super(name);
+    }
+//    @Override
+//    public void start() {
+//        System.out.println("Thread is Running");
+//        for(int i=0;i<5;i++){
+//            try{
+//                Thread.sleep(1000);
+//            }
+//            catch (InterruptedException e){
+//                throw new RuntimeException(e);
+//            }
+//            System.out.println(i);
+//        }
+//    }
+
     @Override
-    public void start() {
-        System.out.println("Thread is Running");
+    public void run() {
         for(int i=0;i<5;i++){
+            System.out.println(Thread.currentThread().getName()+" -Priority"+
+                    Thread.currentThread().getPriority()+" -count"+i);
             try{
-                Thread.sleep(1000);
+                Thread.sleep(100);
             }
-            catch (InterruptedException e){
-                throw new RuntimeException(e);
+            catch (Exception e){
+                e.printStackTrace();
             }
-            System.out.println(i);
         }
     }
 
     public static void main(String[] args) throws InterruptedException{
-        MyThread1 t1=new MyThread1();
-        t1.start();
-        t1.join();
+        MyThread1 t1=new MyThread1("Soumyodip");
+//        t1.start();
+//        t1.join();
         System.out.println("Hello");
+        t1.start();
     }
 }
