305312f610d9c46dbaee03b0a2b13a2c7d39769b
coder <soumyodipsutra@gmail.com>
2025-04-29 07:06:01 +0530

    working

--- src/UberRentalSystem/Admin.java
+++ src/UberRentalSystem/Admin.java
@@ -1,4 +1,36 @@
 package UberRentalSystem;
 
-public class Admin {
+import java.util.Iterator;
+import java.util.List;
+
+@SecurityCheck(role = "Admin")
+public class Admin extends User{
+
+    public Admin(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    void showProfiles() {
+        Class<SecurityCheck> c=SecurityCheck.class;
+        SecurityCheck s=c.getDeclaredAnnotation(SecurityCheck.class);
+        System.out.println(this.name+" "+s.role());
+    }
+
+    void removeDriver(List<Driver> drivers,String driverId) {
+        Class<SecurityCheck> c=SecurityCheck.class;
+        SecurityCheck s=c.getDeclaredAnnotation(SecurityCheck.class);
+        boolean found=true;
+        if(s.role().equals("Admin") && c.isAnnotationPresent(SecurityCheck.class)) {
+            Iterator itr=drivers.iterator();
+            while(itr.hasNext()) {
+                Driver d=(Driver)itr.next();
+                if(d.id==driverId) {
+                    itr.remove();
+                    break;
+                }
+            }
+        }
+    }
+
 }
