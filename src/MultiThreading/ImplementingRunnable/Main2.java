494b754fb4134687b2df2ae60ddbead01fd2a92e
coder <soumyodipsutra@gmail.com>
2025-04-29 18:12:58 +0530

    working

---
+++ src/MultiThreading/ImplementingRunnable/Main2.java
@@ -0,0 +1,17 @@
+package MultiThreading.ImplementingRunnable;
+
+import MultiThreading.ExtendingThread.World1;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        World2 world2=new World2();
+        Thread t2=new Thread(world2);
+        t2.start();
+
+        for(int i=0;i<10000;i++){
+            System.out.println("Hello");
+        }
+    }
+}
