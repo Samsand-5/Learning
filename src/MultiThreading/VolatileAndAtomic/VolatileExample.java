e28dcb58646461489260ab8281559e0cba851d91
coder <soumyodipsutra@gmail.com>
2025-04-30 15:06:57 +0530

    working

---
+++ src/MultiThreading/VolatileAndAtomic/VolatileExample.java
@@ -0,0 +1,12 @@
+package MultiThreading.VolatileAndAtomic;
+
+
+class SharedObj{
+
+    private boolean flag=false;
+}
+public class VolatileExample {
+    public static void main(String[] args) {
+
+    }
+}
