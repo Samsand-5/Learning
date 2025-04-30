03f4cb81946848884a43b21972d058169d4cfa04
coder <soumyodipsutra@gmail.com>
2025-04-30 13:02:14 +0530

    working

--- src/MultiThreading/CountDownLatch/Main13.java
+++ src/MultiThreading/CountDownLatch/Main13.java
@@ -1,16 +1,17 @@
 package MultiThreading.CountDownLatch;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main13 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException,InterruptedException {
         ExecutorService executorService= Executors.newFixedThreadPool(3);
-        Future<String> future=executorService.submit(new DependentService());
         Future<String> future1=executorService.submit(new DependentService());
         Future<String> future2=executorService.submit(new DependentService());
+        Future<String> future3=executorService.submit(new DependentService());
+
+        future1.get();
+        future2.get();
+        future3.get();
     }
 }
 
