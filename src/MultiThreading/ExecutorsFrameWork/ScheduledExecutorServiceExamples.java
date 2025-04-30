930a20b941d06cce2c7996938c9eee13c963cbf4
coder <soumyodipsutra@gmail.com>
2025-04-30 09:23:26 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/ScheduledExecutorServiceExamples.java
+++ src/MultiThreading/ExecutorsFrameWork/ScheduledExecutorServiceExamples.java
@@ -2,9 +2,15 @@ package MultiThreading.ExecutorsFrameWork;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ScheduledExecutorServiceExamples {
     public static void main(String[] args) {
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.schedule(
+                () -> System.out.println("Task executed in 5 second delay"),
+                5,
+                TimeUnit.SECONDS);
+        scheduler.shutdown();
     }
 }
