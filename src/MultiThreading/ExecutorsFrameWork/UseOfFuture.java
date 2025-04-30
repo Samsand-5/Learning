1d03af20d508b3fd80fca2f0d07ec9d77fe7da61
coder <soumyodipsutra@gmail.com>
2025-04-30 09:01:46 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/UseOfFuture.java
+++ src/MultiThreading/ExecutorsFrameWork/UseOfFuture.java
@@ -1,5 +1,7 @@
 package MultiThreading.ExecutorsFrameWork;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class UseOfFuture {
@@ -38,5 +40,16 @@ public class UseOfFuture {
         executorService4.shutdown();
         Thread.sleep(1);
         System.out.println(executorService4.isTerminated());
+
+        ExecutorService executorService5=Executors.newFixedThreadPool(2);
+
+        Callable<Integer> callable1=()->1;
+        Callable<Integer> callable2=()->2;
+        Callable<Integer> callable3=()->3;
+
+        List<Callable<Integer>> list=Arrays.asList(callable1,callable2,callable3);
+        executorService5.invokeAll(list);
+
+        executorService5.shutdown();
     }
 }
