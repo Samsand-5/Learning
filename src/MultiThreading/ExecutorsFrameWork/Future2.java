a74784f42c802bb1893e65919d5eda0d2e38eb35
coder <soumyodipsutra@gmail.com>
2025-04-30 09:20:21 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/Future2.java
+++ src/MultiThreading/ExecutorsFrameWork/Future2.java
@@ -3,7 +3,7 @@ package MultiThreading.ExecutorsFrameWork;
 import java.util.concurrent.*;
 
 public class Future2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executorService= Executors.newSingleThreadExecutor();
         Future<Integer> future=executorService.submit(() ->{
@@ -24,5 +24,27 @@ public class Future2 {
         catch (InterruptedException | ExecutionException | TimeoutException e){
             System.out.println("Exception occurred! "+ e);
         }
+        executorService.shutdown();
+
+        ExecutorService executorService1= Executors.newSingleThreadExecutor();
+        Future<Integer> future1=executorService1.submit(() ->{
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e){
+                System.out.println("Exception occurred! "+e);
+            }
+            return 42;
+        });
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+            System.out.println("Exception occurred! "+e);
+        }
+        future1.cancel(false);//true means it will always get cancelled
+        System.out.println(future1.isCancelled());
+        System.out.println(future1.isDone());
+        executorService1.shutdown();
     }
 }
