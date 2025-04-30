5e41e4c5f849d8c2e8074afb768552e7cb0517e3
coder <soumyodipsutra@gmail.com>
2025-04-30 09:21:58 +0530

    working

---
+++ src/MultiThreading/ExecutorsFrameWork/ScheduledExecutorServiceExamples.java
@@ -0,0 +1,10 @@
+package MultiThreading.ExecutorsFrameWork;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class ScheduledExecutorServiceExamples {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    }
+}
