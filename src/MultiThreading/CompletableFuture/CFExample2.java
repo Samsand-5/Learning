51efdc1dee096595108951f9346ba254103e4300
coder <soumyodipsutra@gmail.com>
2025-04-30 14:55:13 +0530

    working

---
+++ src/MultiThreading/CompletableFuture/CFExample2.java
@@ -0,0 +1,30 @@
+package MultiThreading.CompletableFuture;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CFExample2 {
+    public static void main(String[] args) {
+        CompletableFuture<String> f1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println("worker");
+            } catch (Exception e) {
+
+            }
+            return "ok";
+        });
+
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println("worker");
+            } catch (Exception e) {
+
+            }
+            return "ok";
+        });
+
+        CompletableFuture<Void> f = CompletableFuture.allOf(f1, f2);
+        f.join();
+    }
+}
