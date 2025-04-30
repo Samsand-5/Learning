9a6f946ad8244760bd061f7233da7320e6ca8384
coder <soumyodipsutra@gmail.com>
2025-04-30 15:08:02 +0530

    working

--- src/MultiThreading/VolatileAndAtomic/VolatileExample.java
+++ src/MultiThreading/VolatileAndAtomic/VolatileExample.java
@@ -4,6 +4,17 @@ package MultiThreading.VolatileAndAtomic;
 class SharedObj{
 
     private boolean flag=false;
+
+    public void setFlagTrue(){
+        flag=true;
+    }
+
+    public void printIfFlagTrue(){
+        while(!flag){
+
+        }
+        System.out.println("Flag is true");
+    }
 }
 public class VolatileExample {
     public static void main(String[] args) {
