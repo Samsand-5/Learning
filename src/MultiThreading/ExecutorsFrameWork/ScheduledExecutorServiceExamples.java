096b5d473784564e9956de9487cb75467d22f2ae
coder <soumyodipsutra@gmail.com>
2025-04-30 09:27:48 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/ScheduledExecutorServiceExamples.java
+++ src/MultiThreading/ExecutorsFrameWork/ScheduledExecutorServiceExamples.java
@@ -12,5 +12,17 @@ public class ScheduledExecutorServiceExamples {
                 5,
                 TimeUnit.SECONDS);
         scheduler.shutdown();
+
+        ScheduledExecutorService scheduler1 = Executors.newScheduledThreadPool(1);
+        scheduler1.scheduleAtFixedRate(
+                () -> System.out.println("Task executed after every 5 second delay"),
+                5,
+                5,
+                TimeUnit.SECONDS);
+        scheduler1.schedule(() ->{
+            System.out.println("Initiating shutdown....");
+            scheduler1.shutdown();
+        },20, TimeUnit.SECONDS);
+
     }
 }
