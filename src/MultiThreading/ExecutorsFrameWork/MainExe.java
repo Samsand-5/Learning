1d855bbf1dfcb1051665cfd16af686e30eb57e5b
coder <soumyodipsutra@gmail.com>
2025-04-30 08:17:03 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/MainExe.java
+++ src/MultiThreading/ExecutorsFrameWork/MainExe.java
@@ -1,31 +1,29 @@
 package MultiThreading.ExecutorsFrameWork;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class MainExe {
 
     public static void main(String[] args) {
+//        Executor executor1=Executors.newFixedThreadPool(9);
+//        for(int i=1;i<10;i++) {
+//            int finalI = i;
+//            executor1.execute(  () ->{
+//                long result=factorial(finalI);
+//                System.out.println(result);
+//            });
+//        }
+
+
         ExecutorService executor=Executors.newFixedThreadPool(9);
         for(int i=1;i<10;i++) {
             int finalI = i;
-            executor.submit(  () ->{
+            Future<?>future = executor.submit(  () ->{
                 long result=factorial(finalI);
                 System.out.println(result);
             });
         }
         executor.shutdown();
-
-        Executor executor1=Executors.newFixedThreadPool(9);
-        for(int i=1;i<10;i++) {
-            int finalI = i;
-            executor1.execute(  () ->{
-                long result=factorial(finalI);
-                System.out.println(result);
-            });
-        }
     }
 
     private static long factorial(int finalI) {
