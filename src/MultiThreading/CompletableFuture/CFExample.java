9a432151ff7d84f6a5d1818df0e7d1f509db7d84
coder <soumyodipsutra@gmail.com>
2025-04-30 14:53:16 +0530

    working

--- src/MultiThreading/CompletableFuture/CFExample.java
+++ src/MultiThreading/CompletableFuture/CFExample.java
@@ -1,7 +1,32 @@
 package MultiThreading.CompletableFuture;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+//CompletableFuture is used to run asynchronous tasks
+
 public class CFExample {
     public static void main(String[] args) {
+        CompletableFuture<String> completableFuture=CompletableFuture.supplyAsync(() ->{
+           try{
+               Thread.sleep(5000);
+               System.out.println("worker");
+           }
+           catch (Exception e){
 
+           }
+           return "ok";
+        });
+        String s= null;
+//        try {
+//            s = completableFuture.get();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        } catch (ExecutionException e) {
+//            throw new RuntimeException(e);
+//        }
+        s=completableFuture.getNow("noo");
+        System.out.println(s);
+        System.out.println("Main");
     }
 }
