7a8a80361f73d98fafe1ab9c28dcb636c6bbae4e
coder <soumyodipsutra@gmail.com>
2025-04-30 14:17:02 +0530

    working

--- src/MultiThreading/CountDownLatch/UsingCountDownLatchViaCyclicBarrier.java
+++ src/MultiThreading/CountDownLatch/UsingCountDownLatchViaCyclicBarrier.java
@@ -1,21 +1,39 @@
 package MultiThreading.CountDownLatch;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class UsingCountDownLatchViaCyclicBarrier {
     public static void main(String[] args) throws ExecutionException,InterruptedException {
         int numberOfServices=3;
         ExecutorService executorService= Executors.newFixedThreadPool(numberOfServices);
-        CountDownLatch latch=new CountDownLatch(numberOfServices);
-        executorService.submit(new DependentService(latch));
-        executorService.submit(new DependentService(latch));
-        executorService.submit(new DependentService(latch));
-        latch.await();
+        CyclicBarrier barrier=new CyclicBarrier(numberOfServices);
+        executorService.submit(new DependentService3(barrier));
+        executorService.submit(new DependentService3(barrier));
+        executorService.submit(new DependentService3(barrier));
 
         System.out.println("Main");
         executorService.shutdown();
+    }
+}
+
+class DependentService3 implements Callable<String>{
+
+    private final CyclicBarrier barrier;
+
+    public DependentService3(CyclicBarrier barrier){
+        this.barrier=barrier;
+    }
+
+    @Override
+    public String call() throws Exception {
+        try {
+            System.out.println(Thread.currentThread().getName()+" Service started");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName()+ " is waiting at barrier");
+            barrier.await();
+        }
+        finally {
+        }
+        return "ok";
     }
 }
