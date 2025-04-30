0da405b07f98941f335e30c1a17e254802bee7d4
coder <soumyodipsutra@gmail.com>
2025-04-30 13:47:05 +0530

    working

--- src/MultiThreading/CountDownLatch/Main13.java
+++ src/MultiThreading/CountDownLatch/Main13.java
@@ -2,11 +2,15 @@ package MultiThreading.CountDownLatch;
 
 import java.util.concurrent.*;
 
+//CountDownLatch is used when we need to wait 1 or more than 1 threads to accomplish task
+
 public class Main13 {
     public static void main(String[] args) throws ExecutionException,InterruptedException {
         int numberOfServices=3;
         ExecutorService executorService= Executors.newFixedThreadPool(numberOfServices);
         CountDownLatch latch=new CountDownLatch(numberOfServices);
+
+
     }
 
 }
