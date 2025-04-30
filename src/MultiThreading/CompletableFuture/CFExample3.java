586566348eb17361f0581951dac814881d1ac81e
coder <soumyodipsutra@gmail.com>
2025-04-30 15:00:01 +0530

    working

--- src/MultiThreading/CompletableFuture/CFExample3.java
+++ src/MultiThreading/CompletableFuture/CFExample3.java
@@ -1,10 +1,28 @@
 package MultiThreading.CompletableFuture;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class CFExample3 {
-    public static void main(String[] args) {
-        CompletableFuture<String> f = CompletableFuture.supplyAsync(() -> {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try {
+            String ff = CompletableFuture.supplyAsync(() -> {
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("worker");
+                } catch (Exception e) {
+
+                }
+                return "ok";
+            }).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Main");
+
+        CompletableFuture<String> ff1 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(5000);
                 System.out.println("worker");
@@ -12,6 +30,7 @@ public class CFExample3 {
 
             }
             return "ok";
-        });
+        }).thenApply(x->x+x);
+        System.out.println(ff1.get());
     }
 }
