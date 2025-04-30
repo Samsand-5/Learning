d7c2d48c07371222706c8aa271063e5d1395c67a
coder <soumyodipsutra@gmail.com>
2025-04-30 07:55:00 +0530

    working

--- src/MultiThreading/ExecutorsFrameWork/MainExe.java
+++ src/MultiThreading/ExecutorsFrameWork/MainExe.java
@@ -2,6 +2,32 @@ package MultiThreading.ExecutorsFrameWork;
 
 public class MainExe {
     public static void main(String[] args) {
+        long startTime=System.currentTimeMillis();
+        Thread[] threads=new Thread[9];
+        for(int i=1;i<10;i++) {
+            int finalI = i;
+            threads[i-1]=new Thread(
+                    ()->{
+                        long result=factorial(finalI);
+                        System.out.println(result);
+                    }
+            );
+            threads[i-1].start();
+        }
+        System.out.println("Total time: "+(System.currentTimeMillis()-startTime));
+    }
 
+    private static long factorial(int finalI) {
+        try{
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        long fact=1;
+        for(int i=1;i<=finalI;i++){
+            fact*=i;
+        }
+        return fact;
     }
 }
