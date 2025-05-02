670defffde5c336bbb7682cda9f11ef295f3325b
coder <soumyodipsutra@gmail.com>
2025-05-02 22:43:23 +0530

    working

--- src/MethodChaining/StudentDriver.java
+++ src/MethodChaining/StudentDriver.java
@@ -1,4 +1,10 @@
 package MethodChaining;
 
+import MethodChaining.Student;
+
 public class StudentDriver {
+    public static void main(String[] args) {
+        Student st=new Student();
+        st.setName("Soumyodip").setId(67).display();
+    }
 }
