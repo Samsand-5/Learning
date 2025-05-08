bdb0a222af229ef7fb56032fd9ec94b0ca925e6d
coder <soumyodipsutra@gmail.com>
2025-05-08 17:51:11 +0530

    working

---
+++ src/SwiggyFoodDeliverySystem/Order.java
@@ -0,0 +1,20 @@
+package SwiggyFoodDeliverySystem;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Order {
+    private Customer customer;
+    private DeliveryPerson deliveryPerson;
+    private Map<FoodItem, Integer> itemsOrdered;
+    private String status;
+
+    public Order(Customer customer, Map<String, Integer> requestedItems,
+                 Map<FoodItem, Integer> inventory, List<DeliveryPerson> deliveryPersons) {
+        this.customer = customer;
+        this.status = "Placed";
+        this.itemsOrdered = new HashMap<>();
+
+    }
+}
