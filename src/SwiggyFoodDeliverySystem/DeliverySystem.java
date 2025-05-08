0edb414884875bfd59d7afe8857ee24a0a91eab6
coder <soumyodipsutra@gmail.com>
2025-05-08 18:36:19 +0530

    working

--- src/SwiggyFoodDeliverySystem/DeliverySystem.java
+++ src/SwiggyFoodDeliverySystem/DeliverySystem.java
@@ -1,5 +1,7 @@
 package SwiggyFoodDeliverySystem;
 
+import UberRentalSystem.Driver;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +11,39 @@ public class DeliverySystem {
     private List<Customer> customers = new ArrayList<>();
     private List<DeliveryPerson> deliveryPersons = new ArrayList<>();
     private List<Order> orders = new ArrayList<>();
-    private Map<FoodItem, Integer> inventory = new HashMap<>();
+    private Map<FoodItem, Integer> inventorys = new HashMap<>();
+
+    public void registerCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void registerDeliveryPerson(DeliveryPerson dp) {
+        deliveryPersons.add(dp);
+    }
+
+    public void addFoodItem(FoodItem item, int quantity) {
+        inventorys.put(item, quantity);
+    }
+
+
+    public Order placeOrder(Customer customer,Map<String,Integer> requestedItems) {
+        Order order = new Order(customer,requestedItems,inventorys,deliveryPersons);
+        orders.add(order);
+        return order;
+    }
+
+    public void showAllDeliveryPersons() {
+        for(DeliveryPerson deliveryPerson : deliveryPersons) {
+            deliveryPerson.showProfile();
+        }
+    }
+
+    public Map<FoodItem, Integer> getInventory() {
+        return inventorys;
+    }
+
+    public List<DeliveryPerson> getDeliveryPersons() {
+        return deliveryPersons;
+    }
 
 }
