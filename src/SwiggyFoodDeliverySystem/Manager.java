b20ae7d37916f8227957b3ffdbc81eb2ef4e947b
coder <soumyodipsutra@gmail.com>
2025-05-09 17:32:27 +0530

    working

--- src/SwiggyFoodDeliverySystem/Manager.java
+++ src/SwiggyFoodDeliverySystem/Manager.java
@@ -1,9 +1,19 @@
 package SwiggyFoodDeliverySystem;
 
+import java.util.List;
+
 @RoleCheck(role = "Manager")
 public class Manager extends User{
     Manager(String id, String name) {
         super(id, name);
+    }
+
+    public void removeDeliveryPerson(List<DeliveryPerson> deliveryPersons, String id){
+
+    }
+
+    public void restockItem(List<FoodItem> items, String itemName, int quantity){
+
     }
 
     @Override
