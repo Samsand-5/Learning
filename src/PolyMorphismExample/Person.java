95a432eca2c895881a0515ae8a3248c553120374
coder <soumyodipsutra@gmail.com>
2025-05-11 22:27:53 +0530

    working

---
+++ src/PolyMorphismExample/Person.java
@@ -0,0 +1,15 @@
+package PolyMorphismExample;
+
+public class Person {
+    private String name;
+    private int age;
+
+    Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public void displayInfo() {
+        System.out.print(this.name+" "+this.age);
+    }
+}
