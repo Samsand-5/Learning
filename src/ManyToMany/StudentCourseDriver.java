980f5496030255eda151f204a2035b91f5197a74
coder <soumyodipsutra@gmail.com>
2025-05-13 08:16:37 +0530

    Fixed some bugs

--- src/ManyToMany/StudentCourseDriver.java
+++ src/ManyToMany/StudentCourseDriver.java
@@ -2,6 +2,11 @@ package ManyToMany;
 
 public class StudentCourseDriver {
     public static void main(String[] args) {
+        Student s1=new Student("Soumyodip",13);
+        Student s2=new Student("Shuvam",14);
 
+        Course c1 = new Course("java",5);
+        Course c2 = new Course("Python",8);
+        Course c3 = new Course("C++",2);
     }
 }
