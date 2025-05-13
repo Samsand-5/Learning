4bd58b873546f7bbad5b81b45061f21d14cf0b76
coder <soumyodipsutra@gmail.com>
2025-05-13 12:12:32 +0530

    Fixed some bugs

---
+++ src/EventTicketBookingSystem/Attendee.java
@@ -0,0 +1,13 @@
+package EventTicketBookingSystem;
+
+public class Attendee extends User{
+
+    Attendee(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public void showProfile() {
+        super.showProfile();
+    }
+}
