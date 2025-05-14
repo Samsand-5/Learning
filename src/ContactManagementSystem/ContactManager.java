f96ebf9763f35150c017438fe3bc14c1f8d2da39
coder <soumyodipsutra@gmail.com>
2025-05-14 11:50:35 +0530

    working

--- src/ContactManagementSystem/ContactManager.java
+++ src/ContactManagementSystem/ContactManager.java
@@ -1,5 +1,18 @@
 package ContactManagementSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactManager {
+    private List<Contact> contactList;
+
+    public ContactManager() {
+        contactList = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contactList.add(contact);
+    }
+
 
 }
