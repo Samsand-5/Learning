59eeb10a87c1c8fdae6b8cf7a915d8e3f53d2dda
coder <soumyodipsutra@gmail.com>
2025-05-14 12:53:01 +0530

    working

--- src/ContactManagementSystem/ContactManager.java
+++ src/ContactManagementSystem/ContactManager.java
@@ -14,19 +14,33 @@ public class ContactManager {
         contactList.add(contact);
     }
 
-    public void searchByName(String name){
-        int flag=0;
-        for (Contact contacts:contactList){
-            if(contacts.getName().equalsIgnoreCase(name)){
-                flag=1;
+    public void searchByName(String name) {
+        int flag = 0;
+        for (Contact contacts : contactList) {
+            if (contacts.getName().equalsIgnoreCase(name)) {
+                flag = 1;
                 break;
             }
         }
-        if(flag==1){
-            System.out.println(name+ "is found");
+        if (flag == 1) {
+            System.out.println(name + "is found");
+        } else {
+            System.out.println(name + "is not found");
         }
-        else {
-            System.out.println(name+ "is not found");
+    }
+
+    public void searchByNumber(String number) {
+        int flag = 0;
+        for (Contact contacts : contactList) {
+            if (contacts.getPhoneNumber().equalsIgnoreCase(number)) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            System.out.println(number + "is found");
+        } else {
+            System.out.println(number + "is not found");
         }
     }
 }
